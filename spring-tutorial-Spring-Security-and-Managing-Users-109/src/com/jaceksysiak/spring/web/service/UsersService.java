5cad536591b57fd703af86c4c6b92559351fd77d
j4sysiak <j4sysiak@gmail.com>
2019-03-17 08:41:04 +0100

    1

---
+++ spring-tutorial-Spring-Security-and-Managing-Users-109/src/com/jaceksysiak/spring/web/service/UsersService.java
@@ -0,0 +1,77 @@
+package com.jaceksysiak.spring.web.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.jaceksysiak.spring.web.dao.User;
+import com.jaceksysiak.spring.web.dao.UsersDao;
+
+
+@Service("usersService")
+public class UsersService {
+	
+	private UsersDao usersDao;
+	
+	@Autowired
+	public void setUsersDAO(UsersDao usersDao) {
+		this.usersDao = usersDao;
+	}
+
+ 
+	public void create(User user) {
+		usersDao.create(user);
+		
+	}
+
+	public boolean exists(String username) {
+		
+		return usersDao.exists(username);
+	}
+
+
+	public List<User> getAllUsers() {
+		
+		return usersDao.getAllUsers();
+	}
+
+ 
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
